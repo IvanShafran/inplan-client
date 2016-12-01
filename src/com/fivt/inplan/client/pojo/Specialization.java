283653c246bb39a62283f8e554457eb5a07e7a1e
Ivan Shafran <vanobox07@mail.ru>
2016-12-01 17:30:45 +0700

    напоминание на потом

--- src/com/fivt/inplan/client/pojo/Specialization.java
+++ src/com/fivt/inplan/client/pojo/Specialization.java
@@ -2,4 +2,7 @@ package com.fivt.inplan.client.pojo;
 
 public class Specialization extends BaseItem {
 
+	public Specialization() {
+		throw new UnsupportedOperationException("Не определены остальные поля");
+	}
 }
